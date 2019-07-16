00a2149c04c91620c7675376a0b3c53088e793a2
Naushi-Ali <49521116+naushi-Ali@users.noreply.github.com>
2019-07-16 16:04:30 +0500

    Add files via upload

---
+++ jason/script.js
@@ -0,0 +1,39 @@
+
+
+
+function greet(name){
+return "welcome ,+ name" + "tuba";
+
+}
+
+
+let studentName="Maham";
+let price = 4000;
+
+let  greet =function (name){
+return "welcome,"+name+"tuba";
+
+}
+ greet ("sarah");
+
+ function addTwoNumbers(numb1,numb2){
+    return numb1,numb2;
+   //output using return
+   
+   }
+   
+   
+   // function  call
+   
+   addTwoNumbers(4,9);
+   
+   
+   addTwoNumbers(4,5);
+   
+   addTWONumbers(7,8);
+   
+   x = myFunction(3, 3);
+   
+   function SquareTwoNumbers(P1,P2){
+       return P1,P2; 
+   }
