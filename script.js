9a52037eeda6663fe4a895f8cdd7d7d20027315e
Naushi-Ali <49521116+naushi-Ali@users.noreply.github.com>
2019-07-18 15:56:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function doubleNumber(sherry) {
+    let answer = numb * 2;
+    return answer;
+
+}
+
+function nameCall(name) {
+    let greet = naushi + "eidmubarak";
+    return greet;
+
+}
+
+console.log(nameCall("naushi"));
+
+
+//ahhsjashj
+function countLetters(name) {
+    return name.length;
+}
+
+
+function myFunction() {
+    var str = "Hello World!";
+    var res = str.toUpperCase();
+    document.getElementById("demo").innerHTML = res;
+
+}
+fuction convertToCapitalletters(word)
+return "beenish".touppercase();
+
+function reverseString(str) {
+    // Step 1. Use the split() method to return a new array
+    var splitString = str.split(""); // var splitString = "hello".split("");
+    // ["h", "e", "l", "l", "o"]
+
+    // Step 2. Use the reverse() method to reverse the new created array
+    var reverseArray = splitString.reverse(); // var reverseArray = ["h", "e", "l", "l", "o"].reverse();
+    // ["o", "l", "l", "e", "h"]
+
+    // Step 3. Use the join() method to join all elements of the array into a string
+    var joinArray = reverseArray.join(""); // var joinArray = ["o", "l", "l", "e", "h"].join("");
+    // "olleh"
+
+    //Step 4. Return the reversed string
+    return joinArray; // "olleh"
+}
+
+reverseString("hello");
+
+my anme is sarah 
+
+[].push(arry main add kardyga);
+
+[] .Pop(); method removes the last element from an array.
+
+perseInt ("33");
+
+
+let human =
